02131946f2fdace5c7d8204c29664e76fea51a4c
Brian Gerhards <briangerhards@gmail.com>
2019-02-27 21:27:00 -0600

    fixes existing tests

--- csharp/ApprovalTest.cs
+++ csharp/ApprovalTest.cs
@@ -11,20 +11,25 @@ namespace csharp
         [Test]
         public void ThirtyDays()
         {
-            var lines = File.ReadAllLines("ThirtyDays.txt");
+            var lines = File.ReadAllLines(TestContext.CurrentContext.WorkDirectory + "/ThirtyDays.txt");
 
             StringBuilder fakeoutput = new StringBuilder();
             Console.SetOut(new StringWriter(fakeoutput));
             Console.SetIn(new StringReader("a\n"));
 
             Program.Main(new string[] { });
-            String output = fakeoutput.ToString();
+            var output = fakeoutput.ToString();
 
             var outputLines = output.Split('\n');
             for(var i = 0; i<Math.Min(lines.Length, outputLines.Length); i++) 
             {
-                Assert.AreEqual(lines[i], outputLines[i]);
+                Assert.AreEqual(lines[i], TrimOutputLines(outputLines[i]));
             }
+        }
+
+        private string TrimOutputLines(string OutputLine)
+        {
+            return OutputLine.Replace("\r", "");
         }
     }
 }
