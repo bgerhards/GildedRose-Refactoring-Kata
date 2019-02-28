3e79b8ca6be444cffa64827d7889903c9ea6014b
Brian Gerhards <briangerhards@gmail.com>
2019-02-27 21:49:48 -0600

    beginning to add tests for existing logic

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -7,12 +7,65 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void UpdateQualityShouldDecreaseSellInAndQualityBy1WhenSellInAndQualityIsGreaterThan0()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 4, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            var item = Items[0];
+            AssertItem(item, "foo", 4, 3);
+        }
+
+        [Test]
+        public void UpdateQualityShouldDecreaseSellInAndQualityBy2WhenSellInIs0OrLess()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            var item = Items[0];
+            AssertItem(item, "foo", 3, -1);
+        }
+
+        [Test]
+        public void QualityWillNeverBeLessThan0()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("foo", Items[0].Name);
+
+            var item = Items[0];
+            AssertItem(item, "foo", 0, -1);
+        }
+
+        [Test]
+        public void AgedBrieIncreasesInQualityWithEachUpdate()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            var item = Items[0];
+            AssertItem(item, "Aged Brie", 6, 4);
+        }
+
+        [Test]
+        public void QualityOfATypicalItemNeverExceeds50()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            var item = Items[0];
+            AssertItem(item, "Aged Brie", 50, 4);
+        }
+
+        private void AssertItem(Item item, string itemName, int itemQuality, int itemSellIn)
+        {
+            Assert.AreEqual(itemName, item.Name);
+            Assert.AreEqual(itemQuality, item.Quality);
+            Assert.AreEqual(itemSellIn, item.SellIn);
         }
     }
 }
